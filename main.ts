3f81711496295e5b127944f211ebdc2876db379a
creeze0 <100654370+creeze0@users.noreply.github.com>
2022-03-01 11:00:22 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,18 @@
+input.onButtonPressed(Button.A, function () {
+    target += -1
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showNumber(target)
+    radio.sendValue("id", 8)
+    radio.sendValue("try", target)
+})
+radio.onReceivedString(function (receivedString) {
+    basic.showString(receivedString)
+})
+input.onButtonPressed(Button.B, function () {
+    target += 1
+})
+let target = 0
 basic.showIcon(IconNames.SmallHeart)
+target = 50
+radio.setGroup(99)
